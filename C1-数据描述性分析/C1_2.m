24ebc54704afc4eeee987e32af220584b13ddb60
Shengzhou Qiang <53031172+Alephant6@users.noreply.github.com>
2022-11-15 17:41:27 +0800

    C1.2

---
+++ C1-数据描述性分析/C1_2.m
@@ -0,0 +1,49 @@
+%% C1.2 Analyze Observations
+% Author: Alephant
+% Date: 15 Nov 2022
+clc;
+close all;
+clear;
+
+
+%% load data
+T = readtable('data_1.xls');
+% delete 841
+T(T.ID==841,:) = [];
+
+
+%% paramters
+figpath = 'figures/';
+figtype = '.png';
+variablenames = T.Properties.VariableNames;
+
+
+%% C1.2.1 plotmatrix
+for num = 2:5
+    figure
+    variablename = variablenames{num};
+    figname1 = ['C1.2.1-', variablename];
+
+    plotmatrix(table2array(T(:,num)));
+    title(figname1)
+
+    saveas(gcf, [figpath, figname1, figtype]) 
+end
+
+
+%% C1.2.2 boxplot
+for num = 2:5
+    figure
+    variablename = variablenames{num};
+    figname2 = ['C1.2.2-', variablename];
+
+    boxplot(table2array(T(:,num)));
+    title(figname2)
+
+    saveas(gcf, [figpath, figname2, figtype]) 
+end
+
+
+%% C1.2.3 Pearson correlation coefficient
+A = table2array(T(:,2:5));
+[R, P] = corrcoef(A)
