0ef724e2f19045beeb995ac59e7654e19fd1be04
Shengzhou Qiang <53031172+Alephant6@users.noreply.github.com>
2022-11-15 16:54:04 +0800

    C1.1

---
+++ C1-数据描述性分析/C1_1.m
@@ -0,0 +1,37 @@
+%% C1.1 bivariate normal distribution vector
+% Author: Alephant
+% Date: 15 Nov 2022
+clc;
+close all;
+clear;
+
+
+%% paramters
+n = 500;
+mu = [0; 0];
+Sigmas = {[1, 0; 0, 1]; [0.2, 0; 0, 0.2];
+          [4, 0; 0, 4]; [0.2, 0; 0, 4];
+          [4, 0; 0, 0.2]; [0.2, 0; 0, 4];
+          [0.3, 0.5; 0.5, 4]; [4, 0.5; 0.5, 0.3]         
+          };
+figpath = 'figures/';
+figtype = '.png';
+
+
+%% run
+for num = 1:8
+    figure(num)
+    Sigma = cell2mat(Sigmas(num));
+    figname = ['C1.1.', num2str(num)];
+
+    X = mvnrnd(mu, Sigma, n);
+    y = mvnpdf(X);
+
+    scatter3(X(:,1),X(:,2),y)
+    xlabel('X1')
+    ylabel('X2')
+    zlabel('概率密度')
+    title(figname)
+    
+    saveas(gcf, [figpath, figname, figtype]) 
+end
