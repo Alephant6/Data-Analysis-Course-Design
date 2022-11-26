ad9a8ca1abfb05570e6d35e53944d45be1b79170
Shengzhou Qiang <53031172+Alephant6@users.noreply.github.com>
2022-11-26 17:31:15 +0800

    修改1.2所有
    程序，图片，文档

--- C1-数据描述性分析/C1_2.m
+++ C1-数据描述性分析/C1_2.m
@@ -1,6 +1,6 @@
 %% C1.2 Analyze Observations
 % Author: Alephant
-% Date: 15 Nov 2022
+% Date: 26 Nov 2022
 clc;
 close all;
 clear;
@@ -10,40 +10,26 @@ clear;
 T = readtable('data_1.xls');
 % delete 841
 T(T.ID==841,:) = [];
+A = table2array(T(:,2:5));
 
 
 %% paramters
 figpath = 'figures/';
 figtype = '.png';
-variablenames = T.Properties.VariableNames;
 
 
 %% C1.2.1 plotmatrix
-for i = 2:5
-    figure
-    variablename = variablenames{i};
-    figname1 = ['C1.2.1-', variablename];
-
-    plotmatrix(table2array(T(:,i)));
-    title(figname1)
-
-    saveas(gcf, [figpath, figname1, figtype]) 
-end
+figure
+plotmatrix(A,'*r');
+saveas(gcf, [figpath, 'C1.2.1', figtype]) 
 
 
 %% C1.2.2 boxplot
-for i = 2:5
-    figure
-    variablename = variablenames{i};
-    figname2 = ['C1.2.2-', variablename];
-
-    boxplot(table2array(T(:,i)));
-    title(figname2)
-
-    saveas(gcf, [figpath, figname2, figtype]) 
-end
+figure
+boxplot(A,{'EXPE','QUAL','LOYA','SATI'})
+saveas(gcf, [figpath, 'C1.2.2', figtype]) 
 
 
 %% C1.2.3 Pearson correlation coefficient
-A = table2array(T(:,2:5));
+
 [R, P] = corrcoef(A)
